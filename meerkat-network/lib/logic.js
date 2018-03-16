705f30cc920fb6fc4b26388aad206b488619a5fc
Unknown <nemanja0309@gmail.com>
2018-03-16 18:17:28 +0100

    Created network
    Initial step from composer tutorial

---
+++ meerkat-network/lib/logic.js
@@ -0,0 +1,23 @@
+'use strict';
+/**
+ * Write your transction processor functions here
+ */
+
+/**
+ * Sample transaction
+ * @param {org.meerkat.net.ChangeAssetValue} changeAssetValue
+ * @transaction
+ */
+function onChangeAssetValue(changeAssetValue) {
+    var assetRegistry;
+    var id = changeAssetValue.relatedAsset.assetId;
+    return getAssetRegistry('org.meerkat.net.SampleAsset')
+        .then(function(ar) {
+            assetRegistry = ar;
+            return assetRegistry.get(id);
+        })
+        .then(function(asset) {
+            asset.value = changeAssetValue.newValue;
+            return assetRegistry.update(asset);
+        });
+}
