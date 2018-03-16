18a635291e2ae2cccac82c56954ae0caa572d43e
Ognjen <nemanja0309@gmail.com>
2018-03-16 19:32:12 +0100

    Added transactions for accepting invoice and adding new invoice

--- meerkat-network/lib/logic.js
+++ meerkat-network/lib/logic.js
@@ -5,19 +5,44 @@
 
 /**
  * Sample transaction
- * @param {org.meerkat.net.ChangeAssetValue} changeAssetValue
+ * @param {org.meerkat.net.AcceptInvoice} acceptInvoiceTransaction
  * @transaction
  */
-function onChangeAssetValue(changeAssetValue) {
-    var assetRegistry;
-    var id = changeAssetValue.relatedAsset.assetId;
-    return getAssetRegistry('org.meerkat.net.SampleAsset')
+function onAcceptInvoice(acceptInvoiceTransaction) {
+    let assetRegistry;
+    let id = acceptInvoiceTransaction.invoiceId;
+
+    if(acceptInvoiceTransaction.sender.bizEntityId !== acceptInvoiceTransaction.invoice.receiver.bizEntityId) {
+        throw new Error("Only receiver of transaction can accept transaction");
+    }
+    return getAssetRegistry('org.meerkat.net.Invoice')
         .then(function(ar) {
             assetRegistry = ar;
             return assetRegistry.get(id);
         })
         .then(function(asset) {
-            asset.value = changeAssetValue.newValue;
+            asset.status = acceptInvoiceTransaction.newStatus;
             return assetRegistry.update(asset);
         });
+}
+
+/**
+ * Sample transaction
+ * @param {org.meerkat.net.CreateInvoice} createInvoiceTransaction
+ * @transaction
+ */
+function onCreateInvoice(createInvoiceTransaction) {
+    return getAssetRegistry('org.meerkat.net.Invoice')
+        .then(function(ar) {
+            let factory = getFactory();
+            let newInvoice = factory.newResource("org.meerkat.net", "Invoice", `${createInvoiceTransaction.sender.bizEntityId}_${createInvoiceTransaction.invoiceId}`);
+            newInvoice.amount = createInvoiceTransaction.amount;
+            newInvoice.receiver = createInvoiceTransaction.receiver;
+            newInvoice.sender = createInvoiceTransaction.sender;
+
+            return ar.add(newInvoice);
+        })
+        .catch(function(err){
+            throw new Error(err);
+        })
 }
