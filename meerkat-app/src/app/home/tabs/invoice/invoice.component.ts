4bfc1846178d12d4a2a01bf2420aa85733113d32
Ognjen <kurticognjen@gmail.com>
2018-03-17 13:51:02 +0100

    Add invoice completed

--- meerkat-app/src/app/home/tabs/invoice/invoice.component.ts
+++ meerkat-app/src/app/home/tabs/invoice/invoice.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DataService } from '../../../data.service';
-import { Invoice } from '../../../org.meerkat.net';
+import { Invoice, InvoiceStatus } from '../../../org.meerkat.net';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { InvoiceService } from 'app/services/invoice.service';
 
@@ -10,6 +10,7 @@ import { InvoiceService } from 'app/services/invoice.service';
 	styleUrls: ['./invoice.component.css']
 })
 export class InvoiceComponent {
+	@Input() currentUserId: string;
 
 	myForm: FormGroup;
   	private allAssets;
@@ -27,5 +28,42 @@ export class InvoiceComponent {
 		          amount:this.amount,
 		          receiver:this.receiver
 		    });
+	}
+
+	createInvoiceClicked() {
+		this.asset = {
+			"$class": "org.meerkat.net.Invoice",
+			"invoiceId": this.currentUserId + "_" + this.invoiceId.value,
+			"amount": this.amount.value,
+			"status": "NEW",
+			"sender": "resource:org.meerkat.net.BizEntity#" + this.currentUserId,
+			"receiver": "resource:org.meerkat.net.BizEntity#" + this.receiver.value
+		};
+			
+		this.myForm.setValue({
+			"invoiceId":null,
+			"amount":null,
+			"receiver":null
+		});
+	
+		return this.invoiceService.addAsset(this.asset)
+		.toPromise()
+		.then(() => {
+			this.errorMessage = null;
+			this.myForm.setValue({
+				"invoiceId":null,
+				"amount":null,
+				"receiver":null 
+			});
+			console.log("Invoice successfully created!")
+		})
+		.catch((error) => {
+			if(error == 'Server error'){
+				this.errorMessage = "Could not connect to REST server. Please check your configuration details";
+			}
+			else{
+				this.errorMessage = error;
+			}
+		});
 	}
 }
